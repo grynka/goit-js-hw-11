1f5dd1b6b0ab968c135ff45472877eade366d943
grynka <ihor.kozhemyakin@gmail.com>
2022-10-15 21:19:48 +0300

    upd

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import SimpleLightbox from "simplelightbox";
 // Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
 import Notiflix from "notiflix";
-import axios from "axios";
+import { getImages } from "./js/API";
+import markup from './js/templates/markup.hbs'
+
+
 
 const searchField = document.querySelector("input[name='searchQuery']")
 const searchButton = document.querySelector("button[type='submit']")
@@ -10,7 +13,10 @@ const gallerys = document.querySelector(".gallery")
 
 searchButton.addEventListener("click", function(event) {
     event.preventDefault()
-    getImages(searchField.value)
+    getImages(searchField.value).then(data =>   {
+      Notiflix.Notify.success(`Hooray! We found ${response.data.totalHits} images.`)
+      gallerys.insertAdjacentHTML('beforeend', markup(response.data.hits))
+      })
 })
 
 
@@ -35,36 +41,4 @@ const lightbox = new SimpleLightbox(".gallery a", {
 
 
 
-  async function getImages(text) {
-    console.log(text)
-    try {
-      const response = await axios.get(`https://pixabay.com/api/?key=30502346-d120979d6222d217ab4c63b0e&q=${text}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40`);
-      console.log(response.data.hits);
-      Notiflix.Notify.success(`Hooray! We found ${response.data.totalHits} images.`)
-      gallerys.innerHTML = "";
-      for (hit of response.data.hits) {
-           gallerys.insertAdjacentHTML('beforeend',
-       `<a href="${hit.largeImageURL}"><div class="photo-card">
-  <img src="${hit.webformatURL}" alt="" loading="lazy" />
-  <div class="info">
-    <p class="info-item">
-      <b>Like</b>${hit.likes}
-    </p>
-    <p class="info-item">
-      <b>Views</b>${hit.views}
-    </p>
-    <p class="info-item">
-      <b>Comments</b>${hit.comments}
-    </p>
-    <p class="info-item">
-      <b>Downloads</b>${hit.downloads}
-    </p>
-  </div>
-</div></a>`
-     )
-      }
-    
-    } catch (error) {
-      console.error(error);
-    }
-  }
+  
