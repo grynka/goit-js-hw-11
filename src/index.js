41f935db65b70d3d2c616be94b6b474021f5d48e
grynka <ihor.kozhemyakin@gmail.com>
2022-10-17 12:03:49 +0300

    upd

---
+++ src/index.js
@@ -0,0 +1,47 @@
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import Notiflix from "notiflix";
+import { getImages } from "./js/API";
+import markup from "./js/templates/markup.hbs"
+
+const searchField = document.querySelector("input[name='searchQuery']")
+const searchButton = document.querySelector("button[type='submit']")
+const gallerys = document.querySelector(".gallery")
+const loadMore = document.querySelector(".load-more")
+const lightbox = new SimpleLightbox(".gallery a");
+let page;
+
+searchButton.addEventListener("click", function(event) {
+  page = 1;
+  gallerys.innerHTML = "";
+    event.preventDefault()
+    getImages(searchField.value, page).then(data =>   {
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+      gallerys.insertAdjacentHTML('beforeend', markup(data.hits));
+      lightbox.refresh();
+      const { height: cardHeight } = gallerys
+      .firstElementChild.getBoundingClientRect();
+      window.scrollBy({
+          top: cardHeight * 2,
+            behavior: "smooth",
+          });
+      })
+      loadMore.removeAttribute('hidden');
+
+    })
+
+loadMore.addEventListener("click", onLoad)
+
+function onLoad() {
+ page += 1
+  getImages(searchField.value, page).then(data =>   {
+    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`)
+    gallerys.insertAdjacentHTML('beforeend', markup(data.hits));
+    if(data.totalHits/40 <= page) {
+      Notiflix.Notify.success("We're sorry, but you've reached the end of search results.")
+      loadMore.setAttribute('hidden', true)
+    }
+})
+lightbox.refresh();
+}
--- src/js/API.js
+++ src/js/API.js
@@ -4,10 +4,11 @@ const key = '30502346-d120979d6222d217ab4c63b0e';
 
 export async function getImages(text, page) {
     try {
-   return response = await axios.get(`${URL}?key=${key}&q=${text}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`);
-       }
-  
-  catch (error) {
+  const response = await axios.get(`${URL}?key=${key}&q=${text}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${page}`)
+   return await response.data
+   }
+    
+   catch (error) {
     console.error(error);
   }
 }
